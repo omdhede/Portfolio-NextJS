fc3965670f224cbcbb647acdf4c221e3d9a775a8
linux <omdhede@gmail.com>
2023-11-17 22:57:23 +0530

    Portfolio 2.0

---
+++ sanity/env.js
@@ -0,0 +1,6 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-11-17'
+
+export const dataset = "production";
+export const projectId = "0mbpdxuy";
+export const useCdn = false
