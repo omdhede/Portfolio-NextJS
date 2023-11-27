5eb853bf1355fa47d11b8e859bf7818cf1ca505f
siddharthsutar <ssiddharthsutar@gmail.com>
2023-11-27 21:33:43 +0530

    added fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,62 +31,66 @@ type Props = {
 
 const Home = ({ pageInfo, experiences, socials, skills, projects }: Props) => {
   return (
-    <div className="dark:bg-gradient-to-tr dark:from-[#41565F] dark:to-[#000000] bg-gradient-to-bl from-[#9FFFFF] to-[#FFFFFF] dark:text-white text-black h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-[rgb(36,36,36)] scrollbar-thumb-[#41565F]/80 z-0"
-    >
-      <Head>
-        <title>{pageInfo?.name} - Portfolio</title>
-      </Head>
+		<div className="dark:bg-gradient-to-tr dark:from-[#41565F] dark:to-[#000000] bg-gradient-to-bl from-[#9FFFFF] to-[#FFFFFF] dark:text-white text-black h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-[rgb(36,36,36)] scrollbar-thumb-[#41565F]/80 z-0">
+			<Head>
+				<title>
+					{Array.isArray(pageInfo?.name)
+						? pageInfo?.name.join(" ")
+						: pageInfo?.name}{" "}
+					- Portfolio
+				</title>
+			</Head>
 
-      {/*Header*/}
-      <Header socials={socials} />
+			{/*Header*/}
+			<Header socials={socials} />
 
-      {/*Hero*/}
-      <section id="hero" className="snap-start">
-        <Hero pageInfo={pageInfo} />
-      </section>
+			{/*Hero*/}
+			<section id="hero" className="snap-start">
+				<Hero pageInfo={pageInfo} />
+			</section>
 
-      {/*About*/}
-      <section id="about" className="snap-center">
-        <About pageInfo={pageInfo} />
-      </section>
+			{/*About*/}
+			<section id="about" className="snap-center">
+				<About pageInfo={pageInfo} />
+			</section>
 
-      {/*Experience*/}
-      <section id="experience" className="snap-center">
-        <WorkExperience experiences={experiences} />
-      </section>
+			{/*Experience*/}
+			<section id="experience" className="snap-center">
+				<WorkExperience experiences={experiences} />
+			</section>
 
-      {/*Skills*/}
-      <section id="skills" className="snap-start">
-        <Skills skills={skills}/>
-      </section>
+			{/*Skills*/}
+			<section id="skills" className="snap-start">
+				<Skills skills={skills} />
+			</section>
 
-      {/*Projects*/}
-      <section id="projects" className="snap-center">
-        <Projects projects={projects}/>
-      </section>
+			{/*Projects*/}
+			<section id="projects" className="snap-center">
+				<Projects projects={projects} />
+			</section>
 
-      {/*Contact Me*/}
-      <section id="contact" className="snap-start">
-        <ContactMe pageInfo={pageInfo}/>
-      </section>
+			{/*Contact Me*/}
+			<section id="contact" className="snap-start">
+				<ContactMe pageInfo={pageInfo} />
+			</section>
 
-        <section>
-            <ThemeSwitch />
-        </section>
+			<section>
+				<ThemeSwitch />
+			</section>
 
-      {/*<Link href="#hero">*/}
-      {/*  <footer className="sticky bottom-5 w-full cursor-pointer">*/}
-      {/*    <div className="flex items-center justify-center">*/}
-      {/*      <img*/}
-      {/*        className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"*/}
-      {/*        src={urlFor(pageInfo?.heroImage).url()}*/}
-      {/*        alt=""*/}
-      {/*      />*/}
-      {/*    </div>*/}
-      {/*  </footer>*/}
-      {/*</Link>*/}
-    </div>
-  );
+			{/*<Link href="#hero">*/}
+			{/*  <footer className="sticky bottom-5 w-full cursor-pointer">*/}
+			{/*    <div className="flex items-center justify-center">*/}
+			{/*      <img*/}
+			{/*        className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"*/}
+			{/*        src={urlFor(pageInfo?.heroImage).url()}*/}
+			{/*        alt=""*/}
+			{/*      />*/}
+			{/*    </div>*/}
+			{/*  </footer>*/}
+			{/*</Link>*/}
+		</div>
+	);
 };
 
 export default Home;
