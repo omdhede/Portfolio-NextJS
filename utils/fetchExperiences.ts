cf42572f3cc4c68e0ae8e80c819c356ca7db08f1
siddharthsutar <ssiddharthsutar@gmail.com>
2023-11-28 21:16:49 +0530

    corrected deploy code

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -1,11 +1,23 @@
-import {Experience} from "@/typings";
-export const fetchExperiences = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`);
+import { Experience } from "@/typings";
 
-    const data = await res.json();
-    const experiences: Experience[] = data.experiences;
+export const fetchExperiences = async () => {
+	try {
+		const res = await fetch(
+			`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`
+		);
 
-    // console.log("fetching", experiences);
+		if (!res.ok) {
+			throw new Error(`HTTP error! status: ${res.status}`);
+		}
 
-    return experiences;
-}
+		const data = await res.json();
+		const experiences: Experience[] = data.experiences;
+
+		// console.log("fetching", experiences);
+
+		return experiences;
+	} catch (error) {
+		console.error("An error occurred while fetching the experiences:", error);
+		return [];
+	}
+};
